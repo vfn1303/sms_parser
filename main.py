419dc29613d224bf571ce1d65e54277ff0dc1ace
“Fedor <fedoska123@gmail.com>
2023-07-17 21:50:24 +0300

    fix dialog

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from aiogram.dispatcher.filters import Text
 import pandas as pd
 from dotenv import load_dotenv
 import csv, os, re
+from apscheduler.schedulers.background import BackgroundScheduler
 
 load_dotenv()
 
@@ -59,7 +60,7 @@ async def demo_post(inp: Msg):
     #now = datetime.now()
     message = inp.message
     start_row = re.findall(r'\[.*?\]', message) #[Александр Д] 
-    end_row = re.findall(r'\(.*?\)', message) #(Входящее - 900 )
+    end_row = re.findall(r'\(.*?\)', message) #(Входящее - 900 )f
     message = re.findall(r'\].*?\(', message) #
     data = message[0][2:len(message)-2].split()
     data.insert(0,start_row[0])
@@ -73,28 +74,25 @@ async def demo_post(inp: Msg):
     with open('table.csv', 'a', newline='') as tbl:
         writer = csv.writer(tbl)
         writer.writerow(data)
-        print(tbl)
     return {"error_code": 0}
 
 
 #bot
-acl = (1547884469, 369701464,)
 
+start_kb = ReplyKeyboardMarkup(resize_keyboard=True,)
+start_kb.row('Выгрузить все', 'Получить ссылку для приложения')
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: Message):
+    await message.reply_sticker('CAACAgIAAxkBAAEJty5ktRBJ7cp5zxIthBT1J53_JrG5AwACDg0AAlH5kEqZ_8tFy0kTLC8E', reply_markup=start_kb)   
+
+acl = (1547884469, 369701464,)
 admin_only = lambda message: message.from_user.id not in acl
 
 @dp.message_handler(admin_only, content_types=['any'])
 async def handle_unwanted_users(message: types.Message):
     await bot.delete_message(message.chat.id, message.message_id)
     return
-
-
-start_kb = ReplyKeyboardMarkup(resize_keyboard=True,)
-start_kb.row('Выгрузить все', 'Получить ссылку для приложения')
-
-
-@dp.message_handler(commands=['start'])
-async def cmd_start(message: Message):
-    await message.reply_sticker('CAACAgIAAxkBAAEJty5ktRBJ7cp5zxIthBT1J53_JrG5AwACDg0AAlH5kEqZ_8tFy0kTLC8E', reply_markup=start_kb)    
 
 
 @dp.message_handler(Text(equals=['Выгрузить все'], ignore_case=True))
