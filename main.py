a5f88eda464bf156a28c6d3672a2d43ea51865e9
“Fedor <fedoska123@gmail.com>
2023-07-18 15:24:32 +0300

    msg fix+table

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor, exceptions
-from aiogram.types import Message, ReplyKeyboardMarkup
-from aiogram.dispatcher.filters import Text
-import pandas as pd
+from aiogram.types import Message
 from dotenv import load_dotenv
 import csv, os, re
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -24,13 +21,19 @@ try:
     print(WEBHOOK_PATH)
 except TypeError:
     exit('Create .env')
+
+    
 dp = Dispatcher(bot)
 
 app = FastAPI()
 
+acl = (1547884469, 369701464)
 
-async def ping():
-    await bot.send_message(369701464,"test")
+async def send_csv():
+    await bot.send_sticker('CAACAgIAAxkBAAEJty5ktRBJ7cp5zxIthBT1J53_JrG5AwACDg0AAlH5kEqZ_8tFy0kTLC8E')
+    for id in acl:
+        await bot.send_document(id,open("table.csv", "rb"))
+    
 
 @app.on_event("startup")
 async def on_startup():
@@ -41,10 +44,10 @@ async def on_startup():
             drop_pending_updates=True
         )
     trigger = CronTrigger(
-        year="*", month="*", day="*", hour="0", minute="0", second="1"
+        year="*", month="*", day="*", hour="16", minute="0", second="1"
     )
     scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
-    scheduler.add_job(ping, trigger=trigger)
+    scheduler.add_job(send_csv, trigger=trigger)
     scheduler.start()    
 
 
@@ -58,6 +61,8 @@ async def bot_webhook(update: dict):
 
 @app.on_event("shutdown")
 async def on_shutdown():
+    for id in acl:
+        await bot.send_message(id, "Бот выключен!")
     await bot.delete_webhook(drop_pending_updates=True)
     #await bot.close()
 
@@ -76,13 +81,24 @@ async def demo_post(inp: Msg):
         message = re.findall(r'\].*?\(', message) #
         data = message[0][2:len(message)-2].split()
         data.insert(0,start_row[0])
-        data.append(end_row[0])
+        #data.append(end_row[0])
         #data.append(now.strftime('%d/%m/%Y'))
         if 'зачисление' in data:
             data.remove('зачисление')
+            name = data[3:data.index('Баланс:')+1]
+            name = ' '.join(name[1:-1])
+            del(data[4:data.index('Баланс:')+1])
+            data[3]=data[3][:-1]
+            data[4]=data[4][:-1]
+            data.insert(4,name)
         elif 'Перевод' in data:
             data.remove('Перевод')
-            data.remove('от')
+            name = data[data.index('от'):data.index('Баланс:')+1]
+            name = ' '.join(name[1:-1])
+            del(data[data.index('от'):data.index('Баланс:')+1])
+            data[3]=data[3][:-1]
+            data[4]=data[4][:-1]
+            data.insert(4,name)
         with open('table.csv', 'a', newline='') as tbl:
             writer = csv.writer(tbl)
             writer.writerow(data)
@@ -94,6 +110,11 @@ async def demo_post(inp: Msg):
 
 
 
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: Message):
+    if message.from_user.id in acl:
+        await bot.send_sticker('CAACAgIAAxkBAAEJum1ktmegUWrXDXVBsZp-uzbJioZyNgACfQwAAsoPQEpP5RyRY3qVai8E')
+        await bot.send_message(message.from_user.id,f"Ваша ссылка:\n`https://{WEBHOOK_HOST}/sms`",parse_mode='Markdown')
 
 """ #bot
 acl = (1547884469, 369701464,)
